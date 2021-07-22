677f7c04d40fa3e624e28d1770c1ff424eb09333
legitimately_the_1984th_year_of_the_common_era_&_anno_domini_designations <85363779+literally1984@users.noreply.github.com>
2021-07-22 14:20:00 -0700

    Add files via upload

---
+++ chatreading.py
@@ -0,0 +1,52 @@
+import os
+
+def pydrive2install():
+    os.system("start cmd /c pip install pydrive2")
+
+pydrive2install()
+
+from pydrive2.auth import GoogleAuth
+from pydrive2.drive import GoogleDrive
+
+import time
+
+def cls():
+    print("\f")
+    os.system('cls' if os.name=='nt' else 'clear')
+    
+def startmessagewrite():
+    directory = os.getcwd()
+    cmd = 'start cmd /k "cd ' + directory + '\\Downloads & python chatwriting.py"'
+    os.system(cmd)
+
+gauth = GoogleAuth()
+
+gauth.LoadCredentialsFile("mycreds.txt")
+if gauth.credentials is None:
+    gauth.GetFlow()
+    gauth.flow.params.update({'access_type': 'offline'})
+    gauth.flow.params.update({'approval_prompt': 'force'})
+    gauth.LocalWebserverAuth("localhost",[8080])
+elif gauth.access_token_expired:
+    gauth.Refresh()
+else:
+    gauth.Authorize()
+
+gauth.SaveCredentialsFile("mycreds.txt")
+drive = GoogleDrive(gauth)
+
+startmessagewrite()
+
+filelastmodified = 0
+while True:
+    chatfileforread = drive.CreateFile({"id": "1ZbCPNwm6WRZE5irnAP27L5lye5nppmTS"})
+    if chatfileforread["modifiedDate"] == filelastmodified:
+        time.sleep(1)
+        continue
+    else:
+        cls()
+    chatfileforread.GetContentFile("chatlogsforread.txt")
+    displaychat = open("chatlogsforread.txt", "r")
+    print(displaychat.read())
+    displaychat.close()
+    filelastmodified = chatfileforread["modifiedDate"]         
---
+++ chatwriting.py
@@ -0,0 +1,49 @@
+from pydrive2.auth import GoogleAuth
+from pydrive2.drive import GoogleDrive
+
+import time
+from time import gmtime, strftime
+import os
+
+def cls():
+    print("\f")
+    os.system('cls' if os.name=='nt' else 'clear')
+
+gauth = GoogleAuth()
+gauth.LoadCredentialsFile("mycreds.txt")
+if gauth.credentials is None:
+  gauth.GetFlow()
+  gauth.flow.params.update({'access_type': 'offline'})
+  gauth.flow.params.update({'approval_prompt': 'force'})
+  gauth.LocalWebserverAuth("localhost",[8080])
+elif gauth.access_token_expired:
+  gauth.Refresh()
+else:
+  gauth.Authorize()
+
+gauth.SaveCredentialsFile("mycreds.txt")
+drive = GoogleDrive(gauth)
+
+chatfileforwrite = drive.CreateFile({"id": "1ZbCPNwm6WRZE5irnAP27L5lye5nppmTS"})
+chatfileforwrite.GetContentFile("chatlogsforwrite.txt")
+cls()
+
+userenter = input("Enter your username: ")
+time.sleep(1)
+cls()
+
+while True:
+   time.sleep(1)
+   newmessage = input("Enter your message: \n")
+   cls()
+   newmessage = userenter + ": " + newmessage + "        " + str(strftime("%m-%d-%Y %H:%M:%S", gmtime()) + " GMT")
+   modifyfile = open("chatlogsforwrite.txt", "a")
+   modifyfile.write(newmessage)
+   modifyfile.write("\n")
+   modifyfile.flush()
+   modifyfile.close()
+   modifyfile = open("chatlogsforwrite.txt", "r")
+   logsfordrive = modifyfile.read()
+   modifyfile.close()
+   chatfileforwrite.SetContentString(logsfordrive)
+   chatfileforwrite.Upload()
